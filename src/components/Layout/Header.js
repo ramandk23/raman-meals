79796dfc2c67a01001cb2d875a078c6a552c1d2a
Ramandeep <ramandk23@gmail.com>
2022-11-28 17:07:40 +0800

    Add existing file

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsHeaderImg from '../../assets/meals-header.png';
+import classes from './Header.module.css';
+
+const Header = props => {
+return <Fragment>
+<header className={classes.header}>
+    <h1>Raman-Meals</h1>
+    <HeaderCartButton onClick={props.onShowCart} />
+</header>
+<div className={classes['main-image']}>
+    <img src={mealsHeaderImg} alt = 'A table full of yummy food!'/>
+</div>
+</Fragment>
+};
+
+export default Header;
